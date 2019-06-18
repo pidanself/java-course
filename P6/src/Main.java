1da55df1e3096df4b896f987b56ff8336336bb67
pidanself <15652786161@163.com>
2019-06-18 21:25:06 +0800

    啦啦啦啦

--- P6/src/Main.java
+++ P6/src/Main.java
@@ -1,7 +1,6 @@
-import java.lang.reflect.Executable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+
 
 public class Main implements Runnable{
     sumt s=new sumt();
